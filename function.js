c972da94388c08f9e73fc588a91521c5bd55ba82
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2024-10-30 22:51:27 -0700

    Function that outputs the circumference of a circle given the diameter. function that calls another function

--- function.js
+++ function.js
@@ -2,3 +2,17 @@ function greet(name) {
     return "Hello, " + name + "!"; 
 } 
 // Function that outputs a greeting
+function getCircumference(diameter) {
+     const pi = 3.14159;  
+}
+// Function that gives the approximate circumference of a circle given the diameter
+function firstFunction() {
+     console.log("Inside firstFunction");
+     
+     secondFunction(); 
+     
+     console.log("Back in firstFunction after calling secondFunction"); 
+} 
+    function secondFunction() {
+     console.log("Inside secondFunction");     
+} // it goes from first to the last function
